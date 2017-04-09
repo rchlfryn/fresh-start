9b3ad8eb2dbc61a99292c2dd10eb4b69358b0bb0
Rachel Fryan <rachelafryan@gmail.com>
2017-04-09 11:19:12 -0500

    404 replacement

--- 404.php
+++ 404.php
@@ -1,10 +1,7 @@
 <?php get_header(); ?>
 
-			<div id="content">
-
-				<div id="inner-content" class="wrap cf">
-
-					<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+			<div class="inner-content wrap cf">
+					<main id="main" class="m-all t-all d-all cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
 						<article id="post-not-found" class="hentry cf">
 
@@ -20,24 +17,8 @@
 
 							</section>
 
-							<section class="search">
-
-									<p><?php get_search_form(); ?></p>
-
-							</section>
-
-							<footer class="article-footer">
-
-									<p><?php _e( 'This is the 404.php template.', 'bonestheme' ); ?></p>
-
-							</footer>
-
 						</article>
-
 					</main>
-
-				</div>
-
 			</div>
 
 <?php get_footer(); ?>
